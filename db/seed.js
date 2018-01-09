b138d00ab54c78b49df21a8b133401ccfbfa9c02
Hammad Malik <hmd.node@gmail.com>
2018-01-08 22:37:35 -0500

    Refactor db/seed.js to use .create() vs .collection.insert()
    I prefer this method for seeding, since it makes the database consistent with adding documents
    through CRUD operations.

--- db/seed.js
+++ db/seed.js
@@ -4,6 +4,10 @@ const seedData = require('./seedData.json')
 const Miniature = mongoose.model('Miniature')
 
 Miniature.remove({})
-  .then(_ => Miniature.collection.insert(seedData))
-  .then(_ => process.exit())
+  /* batch insert, the way we were taught. Does not validate seedData */
+  // .then(_ => Miniature.collection.insert(seedData))
+
+  /* less efficient, but validates. the second .then() needs to be after the .create() method because it returns a Promise itself (won't work otherwise)
+  */
+  .then(_ => Miniature.create(seedData).then(_ => process.exit()))
   .catch(error => console.log(error))
